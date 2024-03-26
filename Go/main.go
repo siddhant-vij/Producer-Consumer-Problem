a782c0012d385cf736ada7c323319176bb825062
Siddhant Vij <siddhantvij201@gmail.com>
2024-03-26 19:04:23 +0530

    Go program
    - Leveraging BufferedChannel and goroutines for concurrency patterns.

--- Go/main.go
+++ Go/main.go
@@ -2,8 +2,35 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
+func producer(ch chan int) {
+	i := 1
+	for {
+		ch <- i
+		fmt.Printf("Produced %d\n", i)
+		i++
+		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+	}
+}
+
+func consumer(ch chan int) {
+	for {
+		num := <-ch
+		fmt.Printf("Consumed %d\n", num)
+		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+	}
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	ch := make(chan int, 5)
+
+	go producer(ch)
+	go consumer(ch)
+
+	time.Sleep(2 * time.Second)
+
+	close(ch)
 }
